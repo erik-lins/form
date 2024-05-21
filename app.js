c372d86091b31fe5e66959476b96acf0813d9bc6
erik-lins <58609919+erik-lins@users.noreply.github.com>
2024-05-20 21:35:20 -0300

    changes.s

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
-var express = require('express'),
-path = require ('path'),
-bodyParser = require ('body-parser'),
-cors = require ('cors'),
-mongoose = require ('mongoose');
- 
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var mongoose = require('mongoose');
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/mean', {useNewUrlParser : true, useUnifiedTopology: true}).then(
-() =>{console.log('Database is connected') },
-err => {console.log('Can not connect to the database'+ err)});
- 
-const userRoutes = require ('./routes/user.route');
-const formRoutes = require ('./routes/form.route');
-var app  = express ();
+mongoose.connect('mongodb://127.0.0.1:27017/mean', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => { console.log('Database is connected'); })
+    .catch(err => { console.log('Can not connect to the database', err); });
+
+const userRoutes = require('./routes/user.route');
+const formRoutes = require('./routes/form.route');
+
+var app = express();
 app.use(bodyParser.json());
 app.use(cors());
- 
+
 app.use('/user', userRoutes);
 app.use('/form', formRoutes);
-// app.get('/', function(req,res){
-//     res.send("Hello World!");
-// });
- 
-app.listen(3001, function(){
+
+app.listen(3001, function() {
     console.log('Listening on port 3001');
 });
